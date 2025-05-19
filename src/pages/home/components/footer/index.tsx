433cc3550b67ffcad8939ef05edb759d52b6f916
liuyong.wang.o <liuyonng.wang.o@nio.com>
2025-05-19 13:10:19 +0800

    更新 Footer 组件以传递数据给父组件，并在 Header 组件中添加上下文支持

--- src/pages/home/components/footer/index.tsx
+++ src/pages/home/components/footer/index.tsx
@@ -6,11 +6,11 @@ interface AppProps {
   handleActive: (value: string) => void;
 }
 
-const App: React.FC<AppProps> = ({ handleActive }) => {
+const App: React.FC<AppProps> = ({ name, handleActive }) => {
   return (
     <>
-      <Button onClick={() => handleActive("footer 组件")}>
-        提交数据给父组件
+      <Button onClick={() => handleActive("footer 组件传递的数据")}>
+        提交数据给父组件{name}
       </Button>
     </>
   );
--- src/pages/home/components/header/index.tsx
+++ src/pages/home/components/header/index.tsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
 import "./index.scss";
+import { MessageContext } from "../../index";
 
 interface AppProps {
   left: React.ReactNode;
 }
 
 const App: React.FC<AppProps> = ({ left }) => {
+  const value = useContext(MessageContext);
   return (
     <>
       <div>
         <div className="left">{left}</div>
+        <div className="right">{value}</div>
       </div>
     </>
   );
