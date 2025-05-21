dc7d91e6080e00b8db1032f5177321b9daf6f9d3
liuyong.wang.o <liuyonng.wang.o@nio.com>
2025-05-21 09:23:35 +0800

    feat: 重构 TableList 组件，简化属性传递，移除不必要的样式

--- src/components/TableList/index.tsx
+++ src/components/TableList/index.tsx
@@ -3,16 +3,20 @@ import { Table } from "antd";
 import type { JSX } from "react";
 import type { Props } from "./model";
 
-const App = <T,>({ columns, dataSource }: Props<T>): JSX.Element => (
+const App = <T,>(props: Props<T>): JSX.Element => (
   <div className="table-container">
     <div className="search-container">search</div>
     <div className="tool-container">tool</div>
     <Table
       className="ant-table-container"
-      columns={columns}
-      dataSource={dataSource}
+      {...props}
       virtual={false}
       scroll={{ y: "100%" }}
+      pagination={{
+        showSizeChanger: true,
+        showQuickJumper: true,
+        // position: ["bottomCenter"],
+      }}
     />
   </div>
 );
